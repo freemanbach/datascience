c4ca5637dad7866e6a84e819455b167a873795f7
freemanbach <flo@radford.edu>
2019-09-29 12:57:07 -0400

    added processGPA function

--- ex2/parsedata.py
+++ ex2/parsedata.py
@@ -1,12 +1,24 @@
 import csv
 
+def processGPA(mygpa):
+    size, total = len(mygpa), 0.00
+    for i in mygpa:
+        total+=i
+    avggpa = total / size
+    
+    return avggpa
+
+
 def parseFile():
     
     data = "sampledata.txt"
+    mygpa = []
     
     with open(data) as csvData:
         csv_read = csv.reader(csvData, delimiter=',')
         line = 0
+        print "\n"
+
         for row in csv_read:
             if line == 0:
                 print "Column names are {" +  "".join(row) + "}"
@@ -14,8 +26,14 @@ def parseFile():
             else:
                 #name, class, dorm, room, gpa
                 print  "{ Student " + row[0] + " is in graduating class of " + row[1] + " living in " + row[2] + " room " + row[3] + " and gpa is " + row[4] + " .}"
+                mygpa.append(float(row[4]))
                 line += 1
-        print "Processed: " + str(line) + " lines."
+         
+        gpa = processGPA(mygpa)
+        print "\n"*2
+        print "Your GPA mean is: " + str(gpa)
+        print "Processed: " + str(line) +   " lines."
+        print "Processed: " + str(line-1) + " records."
     
 def main():
     parseFile()
