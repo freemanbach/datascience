02273f734f5870793dc6be1e4cead77d233c0004
freemanbach <flo@radford.edu>
2019-10-17 20:52:09 -0400

    fixed tabbing and added legit main

--- ex5/stddev.py
+++ ex5/stddev.py
@@ -8,22 +8,20 @@ A quick lib for finding out the Standard Deviation of a population
 from a list of values. N is the population.
 """
 def stddevpop(myvalues):
+    n = len(myvalues)
+    sumofvalues, total, mean, std = 0,0,0,0
  
- n = len(myvalues)
- sumofvalues, total, mean, std = 0,0,0,0
+    # find the mean
+    for i in myvalues:
+        sumofvalues+=1
+    mean = sumofvalues / len(myvalues)
  
- # find the mean
- for i in myvalues:
-  sumofvalues+=1
- mean = sumofvalues / len(myvalues)
- 
- # sum of all values from mean
- for i in myvalues:
-  total+=math.pow(i-mean,2)
- 
- std = total / n
- 
- return math.sqrt(std)
+    # sum of all values from mean
+    for i in myvalues:
+        total+=math.pow(i-mean,2)
+    std = total / n
+    
+    return math.sqrt(std)
 
 
 """
@@ -31,34 +29,34 @@ A quick lib for finding out the Standard Deviation of a Sample
 population from a list of values. N-1 is the sample population.
 """
 def stddevsamp(myvalues):
- n = len(myvalues)
- sumofvalues, total, mean, std = 0,0,0,0
+    n = len(myvalues)
+    sumofvalues, total, mean, std = 0,0,0,0
  
- # find the mean
- for i in myvalues:
-  sumofvalues+=1
- mean = sumofvalues / len(myvalues)
+    # find the mean
+    for i in myvalues:
+        sumofvalues+=1
+    mean = sumofvalues / len(myvalues)
  
- # sum of all values from mean
- for i in myvalues:
-  total+=math.pow(i-mean,2)
+    # sum of all values from mean
+    for i in myvalues:
+        total+=math.pow(i-mean,2)
+    std = total / (n-1)
+    
+    return math.sqrt(std)
  
- std = total / (n-1)
- 
- return math.sqrt(std)
- 
- 
-def main():
- mylst = []
- random.seed(time.time())
- for i in range(0,10):
-  tmp = random.randint(0,20)
-  mylst.append(tmp)
-  
- print str(mylst)
- stddev1 = stddevpop(mylst)
- print "Std deviation of a Population: " + str(stddev1)
- stddev2 = stddevsamp(mylst)
- print "Std deviation of a Sample Population: " + str(stddev2)
 
-main()
+def main():
+    mylst = []
+    random.seed(time.time())
+    for i in range(0,10):
+        tmp = random.randint(0,20)
+        mylst.append(tmp)
+        
+    print str(mylst)
+    stddev1 = stddevpop(mylst)
+    print "Std deviation of a Population: " + str(stddev1)
+    stddev2 = stddevsamp(mylst)
+    print "Std deviation of a Sample Population: " + str(stddev2)
+
+if __name__ == "__main__":
+    main()
